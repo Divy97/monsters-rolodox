94befd76b11328d6591723e919659c0de6d23650
Divy <divyparekh1810@gmail.com>
2022-09-06 22:47:25 +0530

    hard coded and displayed monsters name

--- src/App.js
+++ src/App.js
@@ -6,15 +6,39 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {
-        firstName: "Divy",
-        lastName: "Parekh",
-      },
+      monsters: [
+        {
+          name: "Linda",
+          id: 0,
+        },
+        {
+          name: "Frank",
+          id: 1,
+        },
+        {
+          name: "Jacky",
+          id: 2,
+        },
+        {
+          name: "Andre",
+          id: 3,
+        },
+      ],
     };
   }
 
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster, id) => {
+          return (
+            <div key={id}>
+              <h1>{monster.name}</h1>;
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 

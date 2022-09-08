2b5bd94c93d21ea33ce54d05e556990227f7cc36
Divy <divyparekh1810@gmail.com>
2022-09-08 18:36:27 +0530

    added some style

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App extends Component {
         <SearchBox
           onChangeHandler={onSearch}
           placeholder="search monsters..."
-          className="search-box"
+          className="monsters-search-box"
         />
         <CardList monsters={filteredMonsters} />
       </div>
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,20 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster, id) => {
           return (
-            <div key={id}>
-              <h1>{monster.name}</h1>
+            <div key={id} className="card-container">
+              <img
+                alt={`monster-${monster.name}`}
+                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+              />
+              <h2>{monster.name}</h2>
+              <p>{monster.email}</p>
             </div>
           );
         })}
